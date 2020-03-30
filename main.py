856370ac7e1ff45ea0ac6897fd0fdd130a3dbe1e
ArtyomVancyan <artyom.vancyan2000@gmail.com>
2020-03-31 00:15:37 +0400

    @citvy_photo_bot

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def make_artwork(in_file, out_file, text):
     colors = [{"bg_color": "#ffffff", "txt_color": "#000000"}, {"bg_color": "#000000", "txt_color": "#ffffff"}]
     color = random.choice(colors)
     ff = random.randint(1, 10) # random font-family [1-10]
-    fz = H//10 # font-size
+    fz = H//12 # font-size
     font = ImageFont.truetype(f"font-lang/{lang}/{ff}.ttf", fz)
     w, h = font.getsize(text)
 
@@ -57,7 +57,7 @@ def make_artwork(in_file, out_file, text):
         draw.rectangle(((W-w)/2, (H-h)/2, (W-w)/2+w, (H-h)/2+h), fill=color["bg_color"])
         draw.text(xy=((W-w)/2, (H-h)/2-(fz/8)), text=text, fill=color["txt_color"], font=font)
 
-    font = ImageFont.truetype(f"font-lang/English/citvy.ttf", fz)
+    font = ImageFont.truetype(f"font-lang/English/citvy.ttf", fz//2)
     w, h = font.getsize("Citvy")
     draw.text(xy=(W-w-(w/4), H-h-(h/4)), text="Citvy", fill=(0, 0, 0), font=font)
     draw.text(xy=(W-w-(w/4), H-h-(h/4)), text="Citvy", fill=(255, 255, 255), font=font)
