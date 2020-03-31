da811e43e694085b4aa558c1a7195ae1a7ded0fc
vladlypovyi <vladlypovy@ukr.net>
2020-03-31 21:03:20 +0300

    deleting unnecessary line

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.insert(0, 'src')
 import transform, numpy as np, vgg, pdb, os
-import scipy.misc
 import tensorflow as tf
 from utils import save_img, get_img, exists, list_files
 from argparse import ArgumentParser
